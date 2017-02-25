87f8baac291b9f0948d4d23943be36bcc80f0518
marcus <marcus@localhost.localdomain>
2017-02-25 15:04:14 +0100

    Started writining init func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::Path;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    init_mos();
+}
+
+fn init_mos() {
+    let path = Path::new("me.mos");
+
+    if path.exists() {
+        println!("File exists: me.mos");
+    }
+    else {
+        first_use();
+    }
+}
+
+fn first_use() {
+    let mut input: String = String::new();
+    
+    println!("First usage of mos, please fill in user credentials:");
+    print!("Nickname: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input);
 }
