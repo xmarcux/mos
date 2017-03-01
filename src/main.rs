71dd72bfe8e70377fa5cbadc3c8809d82efdd355
Marcus <marcux@marcux.org>
2017-03-01 22:14:47 +0100

    Change exeuting path to absolute

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::path::Path;
 use std::io::Write;
+use std::env;
 
 fn main() {
     init_mos();
 }
 
 fn init_mos() {
-    let path = Path::new("me.mos");
+    let exe_curr = env::current_exe().unwrap();
+    let exe_path_parent = exe_curr.as_path().parent().unwrap();
+    
+    let me_path = format!("{}/{}", exe_path_parent.display(), "me.mos");
+    
+    let path = Path::new(me_path.as_str());
 
     if path.exists() {
         println!("File exists: me.mos");
@@ -21,6 +27,15 @@ fn first_use() {
     
     println!("First usage of mos, please fill in user credentials:");
     print!("Nickname: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input);
+    print!("First name: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input);
+    print!("Surname: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input);
+    print!("Profession: ");
     std::io::stdout().flush().unwrap();
     std::io::stdin().read_line(&mut input);
 }
